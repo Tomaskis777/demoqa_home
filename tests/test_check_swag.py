8a2afde5c8e8639ec563af42b77f411b272204fe
Tom <mr.ivan.ivanovich777@mail.ru>
2024-06-11 21:18:24 +0300

    First commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,21 +1,8 @@
-from selenium.webdriver.common.by import By
-from pages.demoqa import DemoQa
-import time
+from pages.swag_labs import SwagLabs
 
 def test_check_icon(browser):
-    # browser.get('https://www.maxidom.ru/')
-    #
-    # input = browser.find_element(By.NAME, 'USER_PHONE')
-    #
-    # if input is None:
-    #     print('Не найден')
-    # else:
-    #     print('Найден')
 
-    demo_qa_page = DemoQa(browser)
-    demo_qa_page.visit()
-    time.sleep(3)
-    demo_qa_page.click_on_the_icon()
-    time.sleep(3)
-    assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    swag_labs_page = SwagLabs(browser)
+    swag_labs_page.exist_icon()
+    swag_labs_page.find_name()
+    swag_labs_page.find_password()
