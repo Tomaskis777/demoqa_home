40e69a5a8ee5ef49e0cdcfaf337e2ed5dd5d90b7
Tom <mr.ivan.ivanovich777@mail.ru>
2024-06-13 00:27:11 +0300

    First commit

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -19,6 +19,13 @@ def driver():
 
 def test_get_title(driver):
     driver.get("https://demoqa.com/")
-#     title_element = driver.find_element_by_tag_name("© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.").text
-#     text = title_element.text
-#     assert text == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."  # Текст элемента совпадает с атрибутом innerHTML
+
+def click_on_the_btn(self):
+    self.find_element(locator='#app > div > div > div.home-body > div > div:nth-child(1)').click()
+
+def get_text(self):
+    str(self.find_element('Please select an item from left to start practice.').text)
+    if str == 'Please select an item from left to start practice.':
+        return True
+    else:
+        return False
